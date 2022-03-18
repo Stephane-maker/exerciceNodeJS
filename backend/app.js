78406975fbaa4fc50cf4ca10cf2d777c1155b912
stephane-maker <contact@mydevproject.fr>
2022-03-18 03:56:18 +0100

    debut de la construiction de l'api creation de la base de donee MangoDB installation d'Express enregistrement de produit dans la base de donée MangoDb est affichage des produits redirection du produit selectionée par l'user sur une page

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+//importation express avec require 
+const express = require("express");
+//constante application
+const app = express();
+const Thing = require('./models/thing');
+//intercepter les requete post
+app.use(express.json());
+
+//importation de mongoose
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb+srv://admin:rNQackrugLjztUfC@cluster0.v51i2.mongodb.net/cluster0?retryWrites=true&w=majority", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+//middleware general pour les requete 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+app.post('/api/stuff', (req, res, next) => {
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
+    });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
+});
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error }));
+});
+app.get("/api/stuff", (req, res, next) => {
+    Thing.find()
+        .then(things => res.status(200).json(things))
+        .catch(error => res.status(400).json({ error }))
+});
+//exportation d'application pour y accede depuis les autres fichier est sur le serveur node 
+module.exports = app;
