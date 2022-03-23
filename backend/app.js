64466bca7540ac083e8c5f2dadbfe40c0a6eeca9
stephane-maker <contact@mydevproject.fr>
2022-03-23 19:09:45 +0100

    ajout de la suppression et de la modification des object et structuration de l'api

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,14 @@
 const express = require("express");
 //constante application
 const app = express();
-const Thing = require('./models/thing');
+
 //intercepter les requete post
 app.use(express.json());
 
 //importation de mongoose
 const mongoose = require("mongoose");
+//importation router 
+const stuffRoutes = require('./routes/stuff');
 
 mongoose.connect("mongodb+srv://admin:rNQackrugLjztUfC@cluster0.v51i2.mongodb.net/cluster0?retryWrites=true&w=majority", {
         useNewUrlParser: true,
@@ -24,24 +26,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
-        .catch(error => res.status(400).json({ error }));
-});
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({ _id: req.params.id })
-        .then(thing => res.status(200).json(thing))
-        .catch(error => res.status(404).json({ error }));
-});
-app.get("/api/stuff", (req, res, next) => {
-    Thing.find()
-        .then(things => res.status(200).json(things))
-        .catch(error => res.status(400).json({ error }))
-});
+
+//toute les routes sont suviti par stuffRoutes
+app.use('/api/stuff', stuffRoutes);
+
 //exportation d'application pour y accede depuis les autres fichier est sur le serveur node 
 module.exports = app;
